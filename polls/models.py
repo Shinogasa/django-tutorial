8aa52c2b4d7cb5022ffe23cf38133f698595d176
Shinogasa <cuulbulu@gmail.com>
2017-01-28 15:02:05 +0900

    別のチュートリアルその２

--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,3 @@
-from __future__ import unicode_literals
-
 import datetime
 from django.utils import timezone
 
@@ -8,12 +6,19 @@ from django.utils.encoding import python_2_unicode_compatible
 
 # Create your models here.
 class Question(models.Model):
+    class Meta:
+        verbose_name = '質問'
+        verbose_name_plural = '質問の複数形'
+        ordering = ['-pub_date']
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 @python_2_unicode_compatible
 class Choice(models.Model):
