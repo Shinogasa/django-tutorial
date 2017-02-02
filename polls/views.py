82416364a1cff46ba27bd8d860fe4bdb4bac802f
Shinogasa <cuulbulu@gmail.com>
2017-02-02 09:06:24 +0900

    投稿用view編集

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render, Http404
+from django.shortcuts import get_object_or_404, render, Http404, redirect
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.utils.html import mark_safe
@@ -21,4 +21,15 @@ def results(request, question_id):
     return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, pk):
-    pass
+    question = get_object_or_404(Question, pk=pk)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return redirect('index')
