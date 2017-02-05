2a0612a1382907a6230cd8e0bdbd0ebe6d4a14bf
Shinogasa <cuulbulu@gmail.com>
2017-02-05 15:49:38 +0900

    投票用フォームの作成

--- polls/views.py
+++ polls/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.utils.html import mark_safe
 
-from .models import Choice, Question
-from .forms import MyForm
+from .models import Question
+from .forms import MyForm, VoteForm
 
 def index(request):
     return render(request, 'polls/index.html', {
@@ -13,7 +13,15 @@ def index(request):
 
 def detail(request, pk):
     obj = get_object_or_404(Question, pk=pk)
+    if request.method == "POST":
+        form = VoteForm(question=obj, data=request.POST)
+        if form.is_valid():
+            form.vote()
+            return redirect('polls:results', pk)
+    else:
+        form = VoteForm(question=obj)
     return render(request, 'polls/detail.html', {
+        'form': form,
         'question': obj,
     })
 
@@ -23,22 +31,13 @@ def results(request, pk):
         'question': obj,
     })
 
-def vote(request, pk):
-    question = get_object_or_404(Question, pk=pk)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        return render(request, 'polls/detail.html', {
-            'question': question,
-            'error_message': "You didn't select a choice",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        return redirect('index')
-
 def form_test(request):
-    form = MyForm()
+    if request.method == "POST":
+        form = MyForm(data=request.POST)
+        if form.is_valid():
+            pass
+    else:
+        form = MyForm()
     return render(request, 'polls/form.html', {
         'form': form,
     })
