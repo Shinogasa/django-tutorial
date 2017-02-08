11fc5d0647f1ec5224500901215a78e8b1053df8
Shinogasa <cuulbulu@gmail.com>
2017-02-08 09:20:28 +0900

    バグ修正

--- polls/forms.py
+++ polls/forms.py
@@ -1,18 +1,20 @@
 from django import forms
 
+
 class MyForm(forms.Form):
-    text = forms.CharField(max_length=100, required=False, label='あなたの担当は？')
+    text = forms.CharField(max_length=100, required=False, label='テキスト')
+
 
 class VoteForm(forms.Form):
     choice = forms.ModelChoiceField(
         queryset=None,
         label='選択',
-        widget=forms.RadioSelect(),
-        empty_label=None,
         error_messages={
             'required': "You didn't select a choice.",
             'invalid_choice': "invalid choice.",
         },
+        widget=forms.RadioSelect(),
+        empty_label=None,
     )
 
     def __init__(self, question, *args, **kwargs):
--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,28 @@
-from django.shortcuts import get_object_or_404, render, resolve_url
+from django.shortcuts import render
+from django.shortcuts import resolve_url
+from django.shortcuts import get_object_or_404
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic import FormView
 from django.views.generic.detail import SingleObjectMixin
 
+from .forms import MyForm
+from .forms import VoteForm
 from .models import Question
-from .forms import MyForm, VoteForm
+
 
 def index(request):
     return render(request, 'polls/index.html', {
         'questions': Question.objects.all(),
     })
+
+
+class FormTest(FormView):
+    form_class = MyForm
+    template_name = 'polls/form.html'
+    success_url = reverse_lazy('polls:index')
+
+form_test = FormTest.as_view()
+
 
 class Detail(SingleObjectMixin, FormView):
     model = Question
@@ -19,34 +32,29 @@ class Detail(SingleObjectMixin, FormView):
 
     def get(self, request, *args, **kwargs):
         self.object = self.get_object()
-        return super().get(requestm *args, **kwargs)
+        return super().get(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
         self.object = self.get_object()
         return super().post(request, *args, **kwargs)
 
-    def get_from_kwargs(self):
-        kwargs = super().get_from_kwargs()
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
         kwargs['question'] = self.object
         return kwargs
 
-    def from_valid(self, form):
+    def form_valid(self, form):
         form.vote()
-        return super(),form_valid(form)
+        return super().form_valid(form)
 
     def get_success_url(self):
         return resolve_url('polls:results', self.kwargs['pk'])
 
 detail = Detail.as_view()
+
+
 def results(request, pk):
     obj = get_object_or_404(Question, pk=pk)
     return render(request, 'polls/results.html', {
         'question': obj,
     })
-
-class FormTest(FormView):
-    form_class = MyForm
-    template_name = 'polls/form.html'
-    success_url = reverse_lazy('polls:index')
-
-form_test = FormTest.as_view()
