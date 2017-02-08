632087e6bd52f7daba0da932a1cc9214780ca7f7
Shinogasa <cuulbulu@gmail.com>
2017-02-08 14:42:32 +0900

    bootstrapでデザイン修正

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap3',  # ← 追加
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -64,6 +65,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+            ],
+            'builtins': [
+                'bootstrap3.templatetags.bootstrap3', 
             ],
         },
     },
--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import get_object_or_404
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic import FormView
 from django.views.generic.detail import SingleObjectMixin
+from django.contrib import messages
 
 from .forms import MyForm
 from .forms import VoteForm
@@ -45,6 +46,8 @@ class Detail(SingleObjectMixin, FormView):
 
     def form_valid(self, form):
         form.vote()
+        choice = form.cleaned_data['choice']
+        messages.success(self.request, '"%s"に投票しました' % choice)
         return super().form_valid(form)
 
     def get_success_url(self):
